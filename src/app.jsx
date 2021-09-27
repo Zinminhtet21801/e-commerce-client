f57180289d6d343466fbd23006a0811c4e2367df
Zin Min Htet <zetminhtin@gmail.com>
2021-09-27 12:40:53 +0630

    router v4

--- src/app.jsx
+++ src/app.jsx
@@ -3,19 +3,25 @@ import Header from "./components/Header/Header"
 import NavBar from "./components/navbar/navbar"
 import Main from "./components/main/Main"
 import Viewall from "./components/main/Viewall"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import MyAccount from "./components/MyAccount/MyAccount"
+import Cart from "./components/Cart/Cart"
 const App = () =>{
     return (
         <Router>
             <React.Fragment>
                 <NavBar />
-                <Header />
+                <Switch>
                 <Route exact path="/">
+                <Header />
                     <Main title="Women's Clothing" fetch="/women's clothing" />
                     <Main title="Jewelery" fetch="/jewelery" />
                     <Main title="Electronics" fetch="/electronics" />
                 </Route>
                 <Route path="/category/:category" component={Viewall}/>
+                <Route path="/account" component={MyAccount} />
+                <Route path="/cart" component={Cart} />
+                </Switch>
             </React.Fragment>
         </Router>
     )
